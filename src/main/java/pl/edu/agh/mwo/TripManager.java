f876637570d1495d337d098a6a9be2b1b86a5187
Szymon <szgasior@outlook.com>
2018-04-15 14:07:33 +0200

    Implemented TripManager

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -1,5 +1,43 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class TripManager {
+
+	private Collection<Trip> trips;
+
+	public Collection<Trip> getTrips() {
+		return trips;
+	}
+
+	public void addTrip(Trip trip) {
+
+		if (trips == null) {
+			trips = new ArrayList<>();
+		}
+		trips.add(trip);
+	}
+
+	public void removeTrip(Trip trip) {
+
+		trips.remove(trip);
+		if (trips.size() == 0) {
+			trips = null;
+		}
+	}
+
+	public Collection<Trip> findTrip(String keyword) {
+
+		Collection<Trip> foundedTrips = new ArrayList<>();
+
+		for (Trip trip : trips) {
+			String name_and_description = (trip.name() + " " + trip.description()).toLowerCase();
+			if (name_and_description.contains(keyword.toLowerCase())) {
+				foundedTrips.add(trip);
+			}
+		}
+		return foundedTrips;
+	}
 
 }

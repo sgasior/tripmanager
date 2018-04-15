7641e7aa4c1ab648f035fb540e9e189735fd20a8
Szymon <szgasior@outlook.com>
2018-04-15 10:50:55 +0200

    Implementation of the Photo class

--- src/main/java/pl/edu/agh/mwo/Photo.java
+++ src/main/java/pl/edu/agh/mwo/Photo.java
@@ -2,4 +2,17 @@ package pl.edu.agh.mwo;
 
 public class Photo {
 
+	private String comment;
+
+	public Photo(String comment) {
+		this.comment = comment;
+	}
+
+	public String comment() {
+		return getComment();
+	}
+
+	public String getComment() {
+		return comment;
+	}
 }

669d5b65691d18f55e9c3c9ad665046aac3ac8fd
Szymon <szgasior@outlook.com>
2018-04-15 12:11:43 +0200

    Implemented Trip class to pass tests

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -1,5 +1,56 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Trip {
+
+	private String name;
+	private String description;
+	private Collection<Photo> photos;
+
+	public Trip(String name, String description) {
+		if (name == null || description == null || name.equals("") || description.equals("")) {
+			throw new IllegalArgumentException();
+		}
+		this.name = name;
+		this.description = description;
+	}
+
+	public String name() {
+		return getName();
+	}
+
+	public String description() {
+		return getDescription();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Collection<Photo> getPhotos() {
+		return photos;
+	}
+
+	public void addPhoto(Photo photo) {
+
+		if (photos == null) {
+			photos = new ArrayList<>();
+		}
+		photos.add(photo);
+	}
 
 }
